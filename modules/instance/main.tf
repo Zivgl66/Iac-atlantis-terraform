387e18fea372334fe4ba9d4610d670471d29fdcb
ziv.gliser <zivgl66@gmail.com>
2024-07-20 20:22:58 +0300

    changed image on instance.tf

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -1,13 +1,9 @@
-resource "google_compute_instance" "vm_instance" {
+resource "google_compute_instance_from_machine_image" "vm_instance" {
   name         = var.instance_name
-  machine_type = var.instance_type
   zone         = var.instance_zone
 
-  boot_disk {
-    initialize_params {
-      image = var.custom_image
-    }
-  }
+  source_machine_image = var.custom_image
+
 
   network_interface {
     network    = var.vpc_name
@@ -21,6 +17,6 @@ resource "google_compute_instance" "vm_instance" {
 resource "google_compute_instance_group" "instance_group" {
   name       = "${var.instance_name}-group"
   zone       = var.instance_zone
-  instances  = [google_compute_instance.vm_instance.self_link]
-  depends_on = [google_compute_instance.vm_instance]
+  instances  = [google_compute_instance_from_machine_image.vm_instance.self_link]
+  depends_on = [google_compute_instance_from_machine_image.vm_instance]
 }
