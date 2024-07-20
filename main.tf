4e8b46443d23c2b75c7c4bd873b17cd786fd92ec
ziv.gliser <zivgl66@gmail.com>
2024-07-20 16:13:15 +0300

    changes to the tf main

--- main.tf
+++ main.tf
@@ -1,6 +1,28 @@
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state01"
+    prefix  = "terraform/terraform.tfstate"
+  }
+  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.5.0"
+    }
+  }
+}
+
 provider "google" {
+  credentials = file("/home/ziv/git/infratstructure-gitops-project/infrastructure-gitops-project-6137ba1f55bf.json")
   project = "infrastructure-gitops-project"
   region  = "us-east1"
+}
+
+data "google_firestore_document" "lock" {
+  project    = "infrastructure-gitops-project"
+  database   = "tf-state"
+  collection = "terraform_locks"
+  document   = "global_lock"
 }
 
 module "vpc" {
