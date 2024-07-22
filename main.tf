622f513110d9b26d5928cdd6268f6a49ff230608
ziv.gliser <zivgl66@gmail.com>
2024-07-22 09:30:10 +0300

    main

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "instance" {
   vpc_name      = module.vpc.vpc_name
   subnet_name   = module.vpc.private_subnet_name
   project_id = "infrastructure-gitops-project"
-  instance_name = "weather-app-m3"
+  instance_name = "weather-app-m2"
   instance_type = "n1-standard-1"
   instance_zone = "us-east1-c"
   custom_image  = "projects/infrastructure-gitops-project/global/machineImages/weather-app-instance-image"
@@ -66,6 +66,7 @@ module "instance" {
   subnetwork_project  = "infrastructure-gitops-project" 
   depends_on          = [module.vpc] 
 }
+
 
 module "load_balancer" {
   source         = "./modules/load_balancer"
